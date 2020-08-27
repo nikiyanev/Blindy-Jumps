f637c9912308a418e1f462a4adc86ba2db4ee089
Nikolay Yanev <nikiyanev1@gmail.com>
2020-08-27 11:43:59 +0300

    BUG fixedgit status!

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// --VARIABLES--
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 var gameWidth = Number(document.getElementById('game').offsetWidth);
@@ -7,193 +8,132 @@ var html = document.getElementById("html");
 var gameResetH4 = document.getElementById("h4");
 var speedUp = document.getElementById("speedUp");
 var newBoss = document.getElementById("newBoss");
+var alert = document.getElementById("alert");
+var nameGame = document.getElementById("nameGame");
+var totalScoresPerClick = document.getElementById("scoresPerClick");
+var divScoresContainer = document.getElementById("divScoresContainer");
+var yourScoresText = document.getElementById("yourScores");
 
-let speedUpArr = [7, 15, 25, 35, 45, 55, 65, 75];
+let speedUpArr = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100];
+let bossImageArr = ["url(images/scary-face.svg)", "url(images/snail.svg)", "url(images/hair-head.svg)", "url(images/rock.svg)","url(images/mushroom.svg)", "url(images/bomb.svg)"];
+let bossImageCount = 0;
+let blockSpeedUp = 43;
+let scoresPerClick = 10;
 let scores = 0;
 let jumps = 0;
+let left = 0;
 
+// --CHEK WIDTH (DESCTOP or MOBILE)--
 if (gameWidth > 600) {
-    window.onkeydown = function(event){
-        if(event.keyCode === 32) {
-            event.preventDefault();
-            html.click(); 
+    // --CLICK TO CHARACTER JUMP ON DESCTOP--
+window.onkeydown = function(event){
+    if(event.keyCode === 32) {
+        event.preventDefault();
+        html.click(); 
 
-            if (game.style.display != "none") {
-                jumpSound()
-                jumps += 1;
-            }
-            
-            if(character.classList != "animate") {
-                character.classList.add("animate");    
-            }     
+        scores += scoresPerClick;
 
-            if (jumps > 0 && jumps <= 7) {
-                scores += 10;
-                if (jumps === 7) {
-                    block.style.animationDuration = "1.8s";
-                    block.style.backgroundImage = "url(images/snail.svg)";
-                }
-            } else if (jumps > 7 && jumps <= 15) {
-                scores += 20;
-                scoresPerClick.innerText = `${20}`;
-                if (jumps === 15) {
-                    block.style.animationDuration = "1.6s";
-                    block.style.backgroundImage = "url(images/hair-head.svg)";
-                }
-            } else if (jumps > 15 && jumps <= 25) {
-                scores += 30;
-                scoresPerClick.innerText = `${30}`;
-                if (jumps === 25) {
-                    block.style.animationDuration = "1.4s";
-                    block.style.backgroundImage = "url(images/rock.svg)";
-                }
-            } else if (jumps > 25 && jumps <= 35) {
-                scores += 40;
-                scoresPerClick.innerText = `${40}`;
-                if (jumps === 35) {
-                    block.style.animationDuration = "1.2s";
-                    block.style.backgroundImage = "url(images/mushroom.svg)";
-                }
-            } else if (jumps > 35 && jumps <= 45) {
-                scores += 50;
-                scoresPerClick.innerText = `${50}`;
-                if (jumps === 45) {
-                    block.style.animationDuration = "1s";
-                    block.style.backgroundImage = "url(images/scary-face.svg)";
-                }
-            } else if (jumps > 45 && jumps <= 55) {
-                scores += 60;
-                scoresPerClick.innerText = `${60}`;
-                if (jumps === 55) {
-                    block.style.animationDuration = "0.8s";
-                    block.style.backgroundImage = "url(images/bomb.svg)";
-                }
-            } else if (jumps > 55 && jumps <= 65) {
-                scores += 70;
-                scoresPerClick.innerText = `${70}`;
-                if (jumps === 65) {
-                    block.style.animationDuration = "0.6s";
-                }
-            } else if (jumps > 65 && jumps <= 75) {
-                scores += 80;
-                scoresPerClick.innerText = `${80}`;
-                if (jumps === 75) {
-                    block.style.animationDuration = "0.5s";
-                }
-            } else if (jumps > 75) {
-                scores += 90;
-                scoresPerClick.innerText = `${90}`;
-                if (jumps === 85) {
-                    block.style.animationDuration = "0.4s";
-                }
-            }      
-
-            if(speedUpArr.includes(jumps)) {
+        if (game.style.display != "none") {
+            jumpSound()
+            jumps += 1;
+        }
+        // --ADD CLASS TO JUMP--
+        if(character.classList != "animate") {
+            character.classList.add("animate");    
+        }     
+        // --CHEK TO SPEED UP--
+        for (let i = 0; i < speedUpArr.length; i++) {
+            if (speedUpArr[i] === jumps) {
+                clearInterval(slideLeft);
+                blockSpeedUp -= 3;
+                scoresPerClick += 10;
+                slideLeft = setInterval(slideLeftBlock, blockSpeedUp);
+        
                 speedUp.style.display = "block";
-                newBoss.style.display = "block";
                 setTimeout(function() {
                     speedUp.style.display = "none";
-                    newBoss.style.display = "none";
                 }, 1800);
-            } 
-            
-            totalScores.innerText = `${scores}`;
-            totalJumps.innerText = `${jumps}`;       
-
-            setTimeout(function() {
-                character.classList.remove("animate");
-            }, 500);
-           
+            }
         }
-    };
-} else {
-    html.addEventListener('click', (e) => {
-
-        if(character.classList != "animate") {
-            character.classList.add("animate");
-        }
-
-        jumpSound()
-        jumps += 1;
-    
-        if (jumps > 0 && jumps <= 7) {
-            scores += 10;
-            if (jumps === 7) {
-                block.style.animationDuration = "1.8s";
-                block.style.backgroundImage = "url(images/snail.svg)";
-            }
-        } else if (jumps > 7 && jumps <= 15) {
-            scores += 20;
-            scoresPerClick.innerText = `${20}`;
-            if (jumps === 15) {
-                block.style.animationDuration = "1.6s";
-                block.style.backgroundImage = "url(images/hair-head.svg)";
-            }
-        } else if (jumps > 15 && jumps <= 25) {
-            scores += 30;
-            scoresPerClick.innerText = `${30}`;
-            if (jumps === 25) {
-                block.style.animationDuration = "1.4s";
-                block.style.backgroundImage = "url(images/rock.svg)";
-            }
-        } else if (jumps > 25 && jumps <= 35) {
-            scores += 40;
-            scoresPerClick.innerText = `${40}`;
-            if (jumps === 35) {
-                block.style.animationDuration = "1.2s";
-                block.style.backgroundImage = "url(images/mushroom.svg)";
-            }
-        } else if (jumps > 35 && jumps <= 45) {
-            scores += 50;
-            scoresPerClick.innerText = `${50}`;
-            if (jumps === 45) {
-                block.style.animationDuration = "1s";
-                block.style.backgroundImage = "url(images/scary-face.svg)";
-            }
-        } else if (jumps > 45 && jumps <= 55) {
-            scores += 60;
-            scoresPerClick.innerText = `${60}`;
-            if (jumps === 55) {
-                block.style.animationDuration = "0.8s";
-                block.style.backgroundImage = "url(images/bomb.svg)";
-            }
-        } else if (jumps > 55 && jumps <= 65) {
-            scores += 70;
-            scoresPerClick.innerText = `${70}`;
-            if (jumps === 65) {
-                block.style.animationDuration = "0.6s";
-            }
-        } else if (jumps > 65 && jumps <= 75) {
-            scores += 80;
-            scoresPerClick.innerText = `${80}`;
-            if (jumps === 75) {
-                block.style.animationDuration = "0.5s";
-            }
-        } else if (jumps > 75) {
-            scores += 90;
-            scoresPerClick.innerText = `${90}`;
-            if (jumps === 85) {
-                block.style.animationDuration = "0.4s";
-            }
-        }         
-
-        if(speedUpArr.includes(jumps)) {
-            speedUp.style.display = "block";
+        // --CHEK TO BOSS IMAGE CHANGE--
+        if (jumps % 10 === 0 && jumps > 0) {
+            block.style.backgroundImage = bossImageArr[bossImageCount];
             newBoss.style.display = "block";
+            bossImageCount++;
             setTimeout(function() {
-                speedUp.style.display = "none";
                 newBoss.style.display = "none";
             }, 1800);
         } 
-    
+        // --ADD TEXT ON RUNTIME--
         totalScores.innerText = `${scores}`;
-        totalJumps.innerText = `${jumps}`;   
-    
+        totalJumps.innerText = `${jumps}`;       
+        totalScoresPerClick.innerText = `${scoresPerClick}`;
+        // --REMOVE CLASS TO JUMP--
         setTimeout(function() {
             character.classList.remove("animate");
         }, 500);
+        
+    }
+};
+} else {
+    // --CLICK TO CHARACTER JUMP ON MOBILE--
+    html.addEventListener('click', (e) => {
+
+        scores += scoresPerClick;
+
+        jumpSound()
+        jumps += 1;
+         // --ADD CLASS TO JUMP--
+        if(character.classList != "animate") {
+            character.classList.add("animate");    
+        }     
+        // --CHEK TO SPEED UP--
+        for (let i = 0; i < speedUpArr.length; i++) {
+            if (speedUpArr[i] === jumps) {
+                clearInterval(slideLeft);
+                blockSpeedUp -= 3;
+                scoresPerClick += 10;
+                slideLeft = setInterval(slideLeftBlock, blockSpeedUp);
+        
+                speedUp.style.display = "block";
+                setTimeout(function() {
+                    speedUp.style.display = "none";
+                }, 1800);
+            }
+        }
+        // --CHEK TO BOSS IMAGE CHANGE--
+        if (jumps % 10 === 0 && jumps > 0) {
+            block.style.backgroundImage = bossImageArr[bossImageCount];
+            newBoss.style.display = "block";
+            bossImageCount++;
+            setTimeout(function() {
+                newBoss.style.display = "none";
+            }, 1800);
+        } 
+        // --ADD TEXT ON RUNTIME--
+        totalScores.innerText = `${scores}`;
+        totalJumps.innerText = `${jumps}`;       
+        totalScoresPerClick.innerText = `${scoresPerClick}`;
+         // --REMOVE CLASS TO JUMP--
+        setTimeout(function() {
+            character.classList.remove("animate");
+        }, 500);
+
     });
-    
+}
+
+var slideLeft = setInterval(slideLeftBlock, blockSpeedUp);
+
+function slideLeftBlock() {
+
+    if (gameWidth > 600 && left === -40) {
+        left = 1000;
+    } else if (gameWidth < 600 && left === -40){
+        left = 640;
+    }
+
+    left -= 20;
+    block.style.left = left + "px";
 }
 
 var chekDead = setInterval(function() {
@@ -202,28 +142,34 @@ var chekDead = setInterval(function() {
 
     if (gameWidth > 600) {
         if (blockLeft < 120 && blockLeft > 80 && characterTop >= 260) {
-            // crashSound()
+            clearInterval(slideLeft);
+            yourScoresText.innerText = `${scores}`;
             game.style.display = "none";
             gameResetH4.style.display = "none";   
-            alert(`YOU LOSE!`);
+            nameGame.style.display = "none";
+            divScoresContainer.style.display = "block"
+            alert.style.display = "block";
         } 
     } else {
         if (blockLeft < 80 && blockLeft > 40 && characterTop >= 260) {
-            // crashSound()
+            clearInterval(slideLeft);
+            yourScoresText.innerText = `${scores}`;
             game.style.display = "none";
-            gameResetH4.style.display = "none";   
-            alert(`YOU LOSE!`);
+            gameResetH4.style.display = "none"; 
+            nameGame.style.display = "none";  
+            alert.style.display = "block";
+            divScoresContainer.style.display = "block"
         } 
     }
 }, 10);
 
 function jumpSound(){
     // var audio = new Audio("../sounds/jump.mp3");
-    var audio = new Audio("https://raw.githubusercontent.com/nikiyanev/Blindy-Jumps/master/sounds/jump.mp3"); // this is way to play audio file ot github page :)
+    var audio = new Audio("https://raw.githubusercontent.com/nikiyanev/Blindy-Jumps/master/sounds/jump.mp3"); // this is way to play audio file on github page :)
     audio.play();
 }
 
-function crashSound(){
-    var audio = new Audio("../sounds/crash.mp3");
-    audio.play();
-}
+// function crashSound(){
+//     var audio = new Audio("../sounds/crash.mp3");
+//     audio.play();
+// }
