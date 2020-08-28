d7e5f5cdd56e3c85d55a14bc1c09856a8e9cdeac
Nikolay Yanev <nikiyanev1@gmail.com>
2020-08-28 11:23:29 +0300

    adding boss image container

--- script.js
+++ script.js
@@ -6,18 +6,19 @@ var totalScores = document.getElementById("totalScores");
 var totalJumps = document.getElementById("totalJumps"); 
 var html = document.getElementById("html"); 
 var gameResetH4 = document.getElementById("h4");
-var speedUp = document.getElementById("speedUp");
-var newBoss = document.getElementById("newBoss");
+var speedUpText = document.getElementById("speedUp");
+var newBossText = document.getElementById("newBoss");
 var alert = document.getElementById("alert");
 var nameGame = document.getElementById("nameGame");
 var totalScoresPerClick = document.getElementById("scoresPerClick");
 var divScoresContainer = document.getElementById("divScoresContainer");
 var yourScoresText = document.getElementById("yourScores");
+var bossImage = document.getElementById("bossImage");
 
 let speedUpArr = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100];
-let bossImageArr = ["url(images/scary-face.svg)", "url(images/snail.svg)", "url(images/hair-head.svg)", "url(images/rock.svg)","url(images/mushroom.svg)", "url(images/bomb.svg)"];
+let bossImageArr = ["url(images/Bosses/bear.svg)", "url(images/Bosses/minion.svg)", "url(images/Bosses/rock.svg)", "url(images/Bosses/turtle.svg)", "url(images/Bosses/mushroom.svg)", "url(images/Bosses/star.svg)", "url(images/Bosses/bomb.svg)", "url(images/Bosses/goldMushroom.svg)"];
 let bossImageCount = 0;
-let blockSpeedUp = 43;
+let blockSpeedUp = 44;
 let scoresPerClick = 10;
 let scores = 0;
 let jumps = 0;
@@ -31,11 +32,9 @@ window.onkeydown = function(event){
         event.preventDefault();
         html.click(); 
 
-        if (game.style.display != "none") {
-            scores += scoresPerClick;
-        }
-        if (game.style.display != "none") {
+        if (game.style.display != "none") {         
             jumpSound()
+            scores += scoresPerClick;
             jumps += 1;
         }
         // --ADD CLASS TO JUMP--
@@ -46,23 +45,24 @@ window.onkeydown = function(event){
         for (let i = 0; i < speedUpArr.length; i++) {
             if (speedUpArr[i] === jumps) {
                 clearInterval(slideLeft);
-                blockSpeedUp -= 3;
+                blockSpeedUp -= 2;
                 scoresPerClick += 10;
                 slideLeft = setInterval(slideLeftBlock, blockSpeedUp);
         
-                speedUp.style.display = "block";
+                speedUpText.style.display = "block";
                 setTimeout(function() {
-                    speedUp.style.display = "none";
+                    speedUpText.style.display = "none";
                 }, 1800);
             }
         }
         // --CHEK TO BOSS IMAGE CHANGE--
         if (jumps % 10 === 0 && jumps > 0) {
             block.style.backgroundImage = bossImageArr[bossImageCount];
-            newBoss.style.display = "block";
+            bossImage.style.backgroundImage = bossImageArr[bossImageCount];
+            newBossText.style.display = "block";
             bossImageCount++;
             setTimeout(function() {
-                newBoss.style.display = "none";
+                newBossText.style.display = "none";
             }, 1800);
         } 
         // --ADD TEXT ON RUNTIME--
@@ -81,11 +81,11 @@ window.onkeydown = function(event){
     html.addEventListener('click', (e) => {
 
         if (game.style.display != "none") {
+            jumpSound()
+            jumps += 1;
             scores += scoresPerClick;
         }
 
-        jumpSound()
-        jumps += 1;
          // --ADD CLASS TO JUMP--
         if(character.classList != "animate") {
             character.classList.add("animate");    
@@ -94,23 +94,24 @@ window.onkeydown = function(event){
         for (let i = 0; i < speedUpArr.length; i++) {
             if (speedUpArr[i] === jumps) {
                 clearInterval(slideLeft);
-                blockSpeedUp -= 3;
+                blockSpeedUp -= 2;
                 scoresPerClick += 10;
                 slideLeft = setInterval(slideLeftBlock, blockSpeedUp);
         
-                speedUp.style.display = "block";
+                speedUpText.style.display = "block";
                 setTimeout(function() {
-                    speedUp.style.display = "none";
+                    speedUpText.style.display = "none";
                 }, 1800);
             }
         }
         // --CHEK TO BOSS IMAGE CHANGE--
         if (jumps % 10 === 0 && jumps > 0) {
             block.style.backgroundImage = bossImageArr[bossImageCount];
-            newBoss.style.display = "block";
+            bossImage.style.backgroundImage = bossImageArr[bossImageCount];
+            newBossText.style.display = "block";
             bossImageCount++;
             setTimeout(function() {
-                newBoss.style.display = "none";
+                newBossText.style.display = "none";
             }, 1800);
         } 
         // --ADD TEXT ON RUNTIME--
@@ -171,8 +172,3 @@ function jumpSound(){
     var audio = new Audio("https://raw.githubusercontent.com/nikiyanev/Blindy-Jumps/master/sounds/jump.mp3"); // this is way to play audio file on github page :)
     audio.play();
 }
-
-// function crashSound(){
-//     var audio = new Audio("../sounds/crash.mp3");
-//     audio.play();
-// }
